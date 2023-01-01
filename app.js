6d3f0e6c26460ea41335ac1beccbb071f60ebd77
awwallawal <103115087+awwallawal@users.noreply.github.com>
2023-01-01 08:34:33 +0100

    Refactoring the JS Files
    Modified the JS Files Slightly using the If statement

--- app.js
+++ app.js
@@ -6,19 +6,24 @@ const weightEl = document.getElementById('weight-el');
 
 inputBtn.addEventListener('click', function(){
  conversionCalculation();
- alert('Kindly refresh the page if you want to perform another conversion')
-}
+ }
 )
 
 function conversionCalculation(){
  const metersToFeetConversion = 3.222;
  const litersToGallonConversion = 2.134;
  const kilogramToPoundConversion = 1.907;
+
+ if (inputEl.value){
+  distanceEl.textContent += inputEl.value + ' meters' + ' = ' + (inputEl.value*metersToFeetConversion).toFixed(3) + ' feet' + ' | ' + inputEl.value + ' feet' + ' = ' + (inputEl.value/metersToFeetConversion).toFixed(3) + ' meters';
+
+  volumeEl.textContent += inputEl.value + ' liters' + ' = ' + (inputEl.value*litersToGallonConversion).toFixed(3) + ' gallons' + ' | ' + inputEl.value + ' gallons' + ' = ' + (inputEl.value/litersToGallonConversion).toFixed(3) + ' liters';
  
- distanceEl.textContent += inputEl.value + ' meters' + ' = ' + (inputEl.value*metersToFeetConversion).toFixed(3) + ' feet' + ' | ' + inputEl.value + ' feet' + ' = ' + (inputEl.value/metersToFeetConversion).toFixed(3) + ' meters';
-
- volumeEl.textContent += inputEl.value + ' liters' + ' = ' + (inputEl.value*litersToGallonConversion).toFixed(3) + ' gallons' + ' | ' + inputEl.value + ' gallons' + ' = ' + (inputEl.value/litersToGallonConversion).toFixed(3) + ' liters';
-
- weightEl.textContent += inputEl.value + ' kilos' + ' = ' + (inputEl.value*kilogramToPoundConversion).toFixed(3) + ' pounds' + ' | ' + inputEl.value + ' pounds' + ' = ' + (inputEl.value/kilogramToPoundConversion).toFixed(3) + ' kilos';
-
+  weightEl.textContent += inputEl.value + ' kilos' + ' = ' + (inputEl.value*kilogramToPoundConversion).toFixed(3) + ' pounds' + ' | ' + inputEl.value + ' pounds' + ' = ' + (inputEl.value/kilogramToPoundConversion).toFixed(3) + ' kilos';
+ } else{
+  alert('Kindly refresh the page if you want to perform another conversion');
+ }
+ 
+ const resetCount = ""; 
+ inputEl.value = resetCount;
 }
